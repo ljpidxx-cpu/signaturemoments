d4cd09678cb113136007d29d8eecf1a17946f092
ljpidxx-cpu <ljpidxx@gmail.com>
2026-08-03 19:47:42 -0500

    Integrate CartProvider and modify metadata
    Added CartProvider to RootLayout and updated metadata description.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,23 @@
 import "./globals.css";
+import {CartProvider} from "../components/CartContext";
 
 export const metadata = {
-  title: "SignatureMoments",
-  description: "Premium products made simple"
+ title:"SignatureMoments",
+ description:"Premium products"
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({children}){
+
+return(
+<html lang="en">
+<body>
+
+<CartProvider>
+{children}
+</CartProvider>
+
+</body>
+</html>
+);
+
 }
